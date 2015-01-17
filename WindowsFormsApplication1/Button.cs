68b956f83735856af3bdb70083c104195ac91cda
pushnoy <romanenko.igor2014@gmail.com>
2015-01-17 02:33:12 -0500

    wazzzup

---
+++ WindowsFormsApplication1/Button.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+    class Button
+    {
+        public String sign;
+        public Button(String sign)
+        {
+            this.sign = sign;
+        }
+
+        public static String game(int x, int y, String[][] gameArray, Boolean[] check)
+        {
+            if (check[0] == false)
+            {
+                gameArray[x][y] = "X";
+                check[0] = true;
+            } else {
+                gameArray[x][y] = "O";
+                check[0] = false;
+            }
+            return gameArray[x][y];
+        }
+        public static void newGame(String[][] gameArray)
+        {
+            for (int i = 0; i < gameArray.Length; i++)
+            {
+                for(int j = 0; j < gameArray[i].Length; j++)
+                {
+                    gameArray[i][j] = "";
+                }
+            }
+            
+        }
+        public static bool CheckWin(String[][] gameArray)
+        {
+            bool flag = false;
+            for (int i = 0; i < gameArray.Length; i++)
+            {
+                String temp;
+                temp = gameArray[i][0];
+                    for (int j = i; j < gameArray[i].Length; j++)
+                    {
+                        if(temp == gameArray[i][j] && !String.IsNullOrEmpty(gameArray[i][j]))
+                        {
+                            flag = true;
+                        }
+                        else if (temp != gameArray[i][j])
+                        {
+                            flag = false;
+                            break;
+                        }
+                    }
+                    if (flag == true)
+                    {
+                        break;
+                    }
+                                    
+            }
+            return flag;
+        }
+        public static bool verticalCheckWin(String[][] gameArray)
+        {
+            bool flag = false;
+            for (int i = 0; i < gameArray.Length; i++)
+            {
+                String temp = gameArray[0][i];
+                for (int j = 0; j < 3; j++)
+                {
+                    if (temp == gameArray[j][i] && !String.IsNullOrEmpty(gameArray[j][i]))
+                    {
+                        flag = true;
+                    }
+                    else if (temp != gameArray[j][i])
+                    {
+                        flag = false;
+                        break;
+                    }
+                }
+                if (flag == true)
+                {
+                    break;
+                }
+            }
+            return flag;
+        }
+
+        public static bool DiagonalCheckWin1(String[][] gameArray)
+        {
+            bool flag = false;
+          
+            String temp = gameArray[0][0];
+            for(int i = 1; i < gameArray.Length; i++)
+            {
+                if (temp == gameArray[i][i] && !String.IsNullOrEmpty(gameArray[i][i]) && String.IsNullOrEmpty(temp))
+                {
+                    flag = true;
+                }
+                else
+                {
+                    flag = false;
+                    break;
+                }
+            }
+            return flag;
+         }
+        public static bool DiagonalCheckWin2(String[][] gameArray)
+        {
+            bool flag = false;
+            String temp = gameArray[0][2];
+            for (int i = 0; i < gameArray.Length; i++ )
+            {
+                if (gameArray[i][gameArray.Length - 1 - i] == temp && !String.IsNullOrEmpty(gameArray[i][gameArray.Length - 1 - i]) && String.IsNullOrEmpty(temp))
+                {
+                    flag = true;
+                }
+                else
+                {
+                    flag = false;
+                    break;
+                }
+            }
+                return flag;
+        }
+
+   
+    }
+}
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+       String[][] gameArray = { new String[3], new String[3], new String[3] };
+       Boolean[] check = new Boolean[1];
+       String[] tips = { "Ничего себе", "Ничего себе как умно", "Вау", "ОЙ как ты его туда засунул", " Вот так, даа", "О, да детка", "Не так уж круто", "Ништяк", "Абалдеть" };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int x = 0, y = 0;
+            Button.game(x, y, this.gameArray, this.check);
+            richTextBox1.Text = tips[0];
+            button1.Text = this.gameArray[x][y];
+            if (Button.CheckWin(gameArray) || Button.verticalCheckWin(gameArray) || Button.DiagonalCheckWin1(gameArray) || Button.DiagonalCheckWin2(gameArray))
+            {
+                richTextBox1.Text = gameArray[x][y] + " выиграл";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int x = 0, y = 1;
+            Button.game(x, y, this.gameArray, this.check);
+            richTextBox1.Text = tips[1];
+            button2.Text = this.gameArray[x][y];
+            if (Button.CheckWin(gameArray) || Button.verticalCheckWin(gameArray) || Button.DiagonalCheckWin1(gameArray) || Button.DiagonalCheckWin2(gameArray))
+            {
+                richTextBox1.Text = gameArray[x][y] + " выиграл";
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int x = 0, y = 2;
+            Button.game(x, y, this.gameArray, this.check);
+            richTextBox1.Text = tips[2];
+            button3.Text = this.gameArray[x][y];
+            if (Button.CheckWin(gameArray) || Button.verticalCheckWin(gameArray) || Button.DiagonalCheckWin1(gameArray) || Button.DiagonalCheckWin2(gameArray))
+            {
+                richTextBox1.Text = gameArray[x][y] + " выиграл";
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int x = 1, y = 0;
+            Button.game(x, y, this.gameArray, this.check);
+            richTextBox1.Text = tips[3];
+            button4.Text = this.gameArray[x][y];
+            if (Button.CheckWin(gameArray) || Button.verticalCheckWin(gameArray) || Button.DiagonalCheckWin1(gameArray) || Button.DiagonalCheckWin2(gameArray))
+            {
+                richTextBox1.Text = gameArray[x][y] + " выиграл";
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            int x = 1, y = 1;
+            Button.game(x, y, this.gameArray, this.check);
+            richTextBox1.Text = tips[4];
+            button5.Text = this.gameArray[x][y];
+            if (Button.CheckWin(gameArray) || Button.verticalCheckWin(gameArray) || Button.DiagonalCheckWin1(gameArray) || Button.DiagonalCheckWin2(gameArray))
+            {
+                richTextBox1.Text = gameArray[x][y] + " выиграл";
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            int x = 1, y = 2;
+            Button.game(x, y, this.gameArray, this.check);
+            richTextBox1.Text = tips[8];
+            button6.Text = this.gameArray[x][y];
+            if (Button.CheckWin(gameArray) || Button.verticalCheckWin(gameArray) || Button.DiagonalCheckWin1(gameArray) || Button.DiagonalCheckWin2(gameArray))
+            {
+                richTextBox1.Text = gameArray[x][y] + " выиграл";
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            int x = 2, y = 0;
+            Button.game(x, y, this.gameArray, this.check);
+            richTextBox1.Text = tips[5];
+            button7.Text = this.gameArray[x][y];
+            if (Button.CheckWin(gameArray) || Button.verticalCheckWin(gameArray) || Button.DiagonalCheckWin1(gameArray) || Button.DiagonalCheckWin2(gameArray))
+            {
+                richTextBox1.Text = gameArray[x][y] + " выиграл";
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            int x = 2, y = 1;
+            Button.game(x, y, this.gameArray, this.check);
+            richTextBox1.Text = tips[6];
+            button8.Text = this.gameArray[x][y];
+            if (Button.CheckWin(gameArray) || Button.verticalCheckWin(gameArray) || Button.DiagonalCheckWin1(gameArray) || Button.DiagonalCheckWin2(gameArray))
+            {
+                richTextBox1.Text = gameArray[x][y] + " выиграл";
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            int x = 2, y = 2;
+            Button.game(x, y, this.gameArray, this.check);
+            richTextBox1.Text = tips[7];
+            button9.Text = this.gameArray[x][y];
+            if (Button.CheckWin(gameArray) || Button.verticalCheckWin(gameArray) || Button.DiagonalCheckWin1(gameArray) || Button.DiagonalCheckWin2(gameArray))
+            {
+                richTextBox1.Text = gameArray[x][y] + " выиграл";
+            }
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            button1.Text = "";
+            button2.Text = "";
+            button3.Text = "";
+            button4.Text = "";
+            button5.Text = "";
+            button6.Text = "";
+            button7.Text = "";
+            button8.Text = "";
+            button9.Text = "";
+            Button.newGame(gameArray);
+            richTextBox1.Text = "CРАЗИМСЯ!";
+        }
+    }
+}
